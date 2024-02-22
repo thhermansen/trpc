012157c894256460bfd66b0efe24cd0f9db59fc6
Thorbjørn Hermansen <thhermansen@gmail.com>
2024-02-22 10:15:23 +0100

    added more input/output types to big-router to illustrate performance degradation

--- examples/.test/diagnostics-big-router/scripts/codegen-base.ts
+++ examples/.test/diagnostics-big-router/scripts/codegen-base.ts
@@ -2,14 +2,53 @@ import type { TRPCRouterRecord } from '@trpc/server';
 import { z } from 'zod';
 import { publicProcedure } from '~/server/trpc';
 
+
 export const __ROUTER__NAME__ = {
   greeting: publicProcedure
-    .input(
-      z.object({
-        who: z.string(),
-      }),
-    )
-    .query(({ input }) => `hello ${input.who}`),
+  .input(
+    z.object({
+      who: z.string(),
+      who2: z.number(),
+      who3: z.array(z.string()),
+      who4: z.string(),
+      who5: z.string(),
+      who6: z.string(),
+      who7: z.string(),
+      who8: z.string(),
+      who9: z.object({
+        who: z.number(),
+        who2: z.array(z.string()),
+        who3: z.string(),
+        who4: z.string(),
+        who5: z.string(),
+        who6: z.string(),
+        who7: z.string(),
+        who8: z.string(),
+      })
+    }),
+  ).output(
+    z.object({
+      who: z.string(),
+      who2: z.number(),
+      who3: z.array(z.string()),
+      who4: z.string(),
+      who5: z.string(),
+      who6: z.string(),
+      who7: z.string(),
+      who8: z.string(),
+      who9: z.object({
+        who: z.number(),
+        who2: z.array(z.string()),
+        who3: z.string(),
+        who4: z.string(),
+        who5: z.string(),
+        who6: z.string(),
+        who7: z.string(),
+        who8: z.string(),
+      })
+    }),
+  )
+  .query(({ input }) => `hello ${input.who}` as any),
   greeting2: publicProcedure
     .input(
       z.object({
@@ -18,12 +57,50 @@ export const __ROUTER__NAME__ = {
     )
     .query(({ input }) => `hello ${input.who}`),
   greeting3: publicProcedure
-    .input(
-      z.object({
-        who: z.string(),
-      }),
-    )
-    .query(({ input }) => `hello ${input.who}`),
+  .input(
+    z.object({
+      who: z.string(),
+      who2: z.number(),
+      who3: z.array(z.string()),
+      who4: z.string(),
+      who5: z.string(),
+      who6: z.string(),
+      who7: z.string(),
+      who8: z.string(),
+      who9: z.object({
+        who: z.number(),
+        who2: z.array(z.string()),
+        who3: z.string(),
+        who4: z.string(),
+        who5: z.string(),
+        who6: z.string(),
+        who7: z.string(),
+        who8: z.string(),
+      })
+    }),
+  ).output(
+    z.object({
+      who: z.string(),
+      who2: z.number(),
+      who3: z.array(z.string()),
+      who4: z.string(),
+      who5: z.string(),
+      who6: z.string(),
+      who7: z.string(),
+      who8: z.string(),
+      who9: z.object({
+        who: z.number(),
+        who2: z.array(z.string()),
+        who3: z.string(),
+        who4: z.string(),
+        who5: z.string(),
+        who6: z.string(),
+        who7: z.string(),
+        who8: z.string(),
+      })
+    }),
+  )
+  .query(({ input }) => `hello ${input.who}` as any),
   greeting4: publicProcedure
     .input(
       z.object({
@@ -32,12 +109,50 @@ export const __ROUTER__NAME__ = {
     )
     .query(({ input }) => `hello ${input.who}`),
   greeting5: publicProcedure
-    .input(
-      z.object({
-        who: z.string(),
-      }),
-    )
-    .query(({ input }) => `hello ${input.who}`),
+  .input(
+    z.object({
+      who: z.string(),
+      who2: z.number(),
+      who3: z.array(z.string()),
+      who4: z.string(),
+      who5: z.string(),
+      who6: z.string(),
+      who7: z.string(),
+      who8: z.string(),
+      who9: z.object({
+        who: z.number(),
+        who2: z.array(z.string()),
+        who3: z.string(),
+        who4: z.string(),
+        who5: z.string(),
+        who6: z.string(),
+        who7: z.string(),
+        who8: z.string(),
+      })
+    }),
+  ).output(
+    z.object({
+      who: z.string(),
+      who2: z.number(),
+      who3: z.array(z.string()),
+      who4: z.string(),
+      who5: z.string(),
+      who6: z.string(),
+      who7: z.string(),
+      who8: z.string(),
+      who9: z.object({
+        who: z.number(),
+        who2: z.array(z.string()),
+        who3: z.string(),
+        who4: z.string(),
+        who5: z.string(),
+        who6: z.string(),
+        who7: z.string(),
+        who8: z.string(),
+      })
+    }),
+  )
+  .query(({ input }) => `hello ${input.who}` as any),
   childRouter: {
     hello: publicProcedure.query(() => 'there'),
     doSomething: publicProcedure.mutation(() => 'okay'),
